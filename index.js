04ac765ed22928b1646b8b547bb0646f3073eee4
Vinod Kotagiri <vinodkotagiri@icloud.com>
2022-10-17 10:18:34 +0530

    added basic structure of the server

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+//Import Routes
+const authRoutes = require('./routes/auth')
+const categoryRoutes = require('./routes/category')
+const postRoutes = require('./routes/post')
+const websiteRoutes = require('./routes/website')
+
+//Create Serverr
+const app = express()
+const http = require('http').createServer(app)
+
+// Db connection
+mongoose
+	.connect(process.env.DB_URL)
+	.then(() => console.log('DB connected'))
+	.catch((err) => console.log('Error econnecting to database ', err))
+
+// middlewares
+app.use(express.json({ limit: '4mb' }))
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(morgan('dev'))
+
+// route middlewares
+app.use('/api', authRoutes)
+app.use('/api', categoryRoutes)
+app.use('/api', postRoutes)
+app.use('/api', websiteRoutes)
+
+const port = process.env.PORT || 8000
+
+http.listen(port, () => console.log('Server running on port +', port))
